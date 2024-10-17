6f8137deb0a3f8b041a7b8449792c34d051481ce
JAAR <joe.aaron.acosta@outlook.com>
2024-10-17 14:05:55 -0700

    Prompt for the guessing game

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/* Programmer: JAAR
+*  Assignment Chapter: 5 question 18
+*   Date modified: 10/17/2024
+*   IDE/Tool used: IntelliJ
+*/
+
+// Creates an instance of a guessing game where a random number is generated
+// and the user has to try to guess it.
+
+/*
+* Random Number Guessing Game Enhancement
+ Enhance the program that you wrote for Programming Challenge 17 so that it
+ * keeps a count of the number of guesses that the user makes. When the user
+ * correctly guesses the random number, the program should display the number
+ *  of guesses.
+* */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
